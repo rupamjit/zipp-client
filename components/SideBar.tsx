2dc0d369479aabaf4bb161cffc068de5bd2d83ba
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-08-01 00:10:52 +0530

    sidebar ui

---
+++ components/SideBar.tsx
@@ -0,0 +1,65 @@
+import { Bell, Bookmark, Dribbble, Mail, Search, User } from "lucide-react";
+import { House } from "lucide-react";
+
+interface SidebarItemsTypes {
+  title: string;
+  icon: React.ReactNode;
+}
+
+const SidebarItems: SidebarItemsTypes[] = [
+  {
+    title: "Home",
+    icon: <House />,
+  },
+  {
+    title: "Explore",
+    icon: <Search />,
+  },
+  {
+    title: "Notifications",
+    icon: <Bell />,
+  },
+  {
+    title: "Messages",
+    icon: <Mail />,
+  },
+  {
+    title: "Bookmarks",
+    icon: <Bookmark />,
+  },
+  {
+    title: "Profile",
+    icon: <User />,
+  },
+];
+
+const SideBar = () => {
+  return (
+    <div className="col-span-3 pt-8 px-3">
+      <div className="flex  h-fit items-center gap-1 cursor-pointer">
+        <Dribbble size={50} className=" text-blue-600" />
+        <span className="text-5xl font-extrabold">Quizz</span>
+      </div>
+      <div className="mt-4 text-2xl font-bold">
+        <ul>
+          {SidebarItems.map((item, _idx) => (
+            <li
+              className="flex justify-start hover:bg-neutral-700 cursor-pointer w-fit rounded-4xl px-4 py-2  items-center gap-2"
+              key={_idx}
+            >
+              <span>{item.icon}</span>
+              <span>{item.title}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex items-center justify-center mt-4 ">
+        <button className="bg-white w-full text-2xl text-black mx-3 font-semibold py-3 cursor-pointer rounded-full ">
+          Post
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
